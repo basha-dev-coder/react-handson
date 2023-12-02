8fadbf15528a23d4ceadd83773d965144e38b928
barkathbasha <barkathbasha423@gmail.com>
2023-12-02 00:44:12 +0000

    update main.tsx file path

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -9,6 +9,9 @@ function ListGroup() {
           <li
             className="list-group-item"
             key={index}
+            // calling an anoymonus function
+            // we can pass variable in the function to capture what event is triggerd
+            // In browser it will display as Syntheticevent , compaitable with all browsers
             onClick={() => console.log("Clicked ", { item }, { index })}
           >
             {item}
