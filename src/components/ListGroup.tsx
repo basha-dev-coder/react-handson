de9dad32edae13fe753716ad05b0ec8ab75a9c7a
barkathbasha <barkathbasha423@gmail.com>
2023-12-02 17:56:43 +0000

    Handling values using Props

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,27 @@
-function ListGroup() {
-  const items = ["Nottingham", "Leicester", "Birmingham", "Manschester"];
+import { useState } from "react";
 
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({ items, heading }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
-      <h1>List Component</h1>
+      <h1>{heading}</h1>
       <ul className="list-group">
+        <span>{items.length === 0 && "No Item Found!"}</span>
         {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={`list-group-item ${
+              selectedIndex === index ? "active" : ""
+            }`}
             key={index}
             // calling an anoymonus function
             // we can pass variable in the function to capture what event is triggerd
-            // In browser it will display as Syntheticevent , compaitable with all browsers
-            onClick={() => console.log("Clicked ", { item }, { index })}
+            // In browser it will display as SyntheticBaseEvent , compaitable with all browsers
+            onClick={() => setSelectedIndex(index)}
           >
             {item}
           </li>
